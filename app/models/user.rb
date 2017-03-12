a08490e65e5ffe0b0c6ee461c9b7532b04a69a36
Andrew Garde Joia <drewdrewthis@gmail.com>
2017-03-12 17:39:04 -0400

    Update user signup and edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase }
-  validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  before_save { self.email = email.downcase }
+  has_secure_password
+
+  validates :name,  presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
   validates :password, length: { minimum: 6 }
-  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,27 +7,42 @@ RSpec.describe User, type: :model do
     subject.name = 'Example'
     subject.email = 'user@example.com'
     subject.password = 'foobar'
-    # subject.password_confirmation = 'foobar'
+    subject.password_confirmation = 'foobar'
+
     expect(subject).to be_valid
   end
 
   it 'is not valid without a name' do
     subject.email = 'user@example.com'
+    subject.password = 'foobar'
+    subject.password_confirmation = 'foobar'
+
     expect(subject).not_to be_valid
   end
 
   it 'name should not be too long' do
     subject.name = 'a' * 51
+    subject.email = 'user@example.com'
+    subject.password = 'foobar'
+    subject.password_confirmation = 'foobar'
+
     expect(subject).not_to be_valid
   end
 
   it 'is not valid without an email' do
     subject.name = 'Example'
+    subject.password = 'foobar'
+    subject.password_confirmation = 'foobar'
+
     expect(subject).not_to be_valid
   end
 
   it 'is not valid without a valid email' do
+    subject.name = 'name'
     subject.email = 'werwer'
+    subject.password = 'foobar'
+    subject.password_confirmation = 'foobar'
+
     expect(subject).not_to be_valid
   end
 
@@ -35,8 +50,11 @@ RSpec.describe User, type: :model do
     subject.name = 'Example'
     subject.email = 'user@example.com'
     subject.password = 'a' * 5
+    subject.password_confirmation = 'a' * 5
+
     expect(subject).not_to be_valid
     subject.password = 'a' * 50
+    subject.password_confirmation = 'a' * 50
     expect(subject).to be_valid
   end
 end
