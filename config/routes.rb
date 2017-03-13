4e52dcd28f481e5e82d55e60c92f4c9a2a53335d
Andrew Garde Joia <drewdrewthis@gmail.com>
2017-03-12 21:50:18 -0400

    Rename welcome -> dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'dashboard#index'
   # User Management
   get '/signup', to: 'users#new'
   # Static Pages
